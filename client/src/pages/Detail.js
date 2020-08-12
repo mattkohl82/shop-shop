c03fa3a018eac0e19ff848bea57be7644dc8b92e
mattkohl82 <mattkohl82@gmail.com>
2020-08-12 14:47:16 -0500

    lesson 2

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,31 +2,61 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+import Cart from '../components/Cart';
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
-const { id } = useParams();
+  const { id } = useParams();
 
-const [currentProduct, setCurrentProduct] = useState({})
+  const [currentProduct, setCurrentProduct] = useState({})
 
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-const { products } = state;
+  const { products, cart } = state;
 
-useEffect(() => {
-  if (products.length) {
-    setCurrentProduct(products.find(product => product._id === id));
-  } else if (data) {
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+  
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
-  }
-}, [products, data, dispatch, id]);
+  };
+
+  useEffect(() => {
+    if (products.length) {
+      setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+    }
+  }, [products, data, dispatch, id]);
 
   return (
     <>
@@ -46,10 +76,11 @@ useEffect(() => {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
-              Add to Cart
-            </button>
-            <button>
+            <button onClick={addToCart}>Add to cart</button>
+            <button 
+              disabled={!cart.find(p => p._id === currentProduct._id)} 
+              onClick={removeFromCart}
+            >
               Remove from Cart
             </button>
           </p>
@@ -63,6 +94,7 @@ useEffect(() => {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
